2582c1f28ed97d72d0dc995e369cb2da57222524
markoandjel <60831775+markoandjel@users.noreply.github.com>
2023-01-19 22:26:31 +0100

    Nesto novo

--- Projekat/AirportProject/Controllers/AirportController.cs
+++ Projekat/AirportProject/Controllers/AirportController.cs
@@ -11,12 +11,15 @@ using Neo4j.Driver;
 using Neo4jClient;
 using Neo4jClient.Cypher;
 using System.Windows.Forms;
+using NHibernate.Loader.Custom;
+using NHibernate.Mapping;
+using NHibernate.Util;
 
 namespace AirportProject.Controllers
 {
     public class AirportController:Neo4jConnect
     {
-        private IDriver _driver;
+        private readonly IDriver _driver;
         public AirportController()
         { 
 
@@ -31,11 +34,10 @@ namespace AirportProject.Controllers
             var session = _driver.Session(conf => conf
             .WithDefaultAccessMode(AccessMode.Write)
             .WithDatabase("airport"))
-            .Run("CREATE (a:Airport {name: $name,city: $city,code: $code})", new { name = a.Name, code = a.Code, city = a.City });
-
-
+            .Run("MERGE (a:Airport {name: $name,city: $city,code: $code})", new { name = a.Name, code = a.Code, city = a.City });
         }
 
+        [Obsolete]
         public List<Airport> GetAllAirports() 
         {
             /*var session = Connector.Driver.Session();
@@ -52,8 +54,32 @@ namespace AirportProject.Controllers
 
             var session = _driver.Session(conf => conf
             .WithDefaultAccessMode(AccessMode.Read)
-            .WithDatabase("airport"))
-            .Run("MATCH(p:Airport) return p LIMIT 3");
+            .WithDatabase("airport"));
+
+
+            var res = session.ReadTransaction(tx =>
+            {
+                var cursor = tx.Run(@"MATCH(n: Airport) RETURN n.city as city LIMIT 25");
+                var records = cursor.ToList();
+                return records.Select(x=>x["city"].As<string>());
+            });
+            var lista = res.ToList();
+
+
+            var res2 = session.ReadTransaction(tx =>
+            {
+                var cursor = tx.Run(@"MATCH(n: Airport) RETURN n");
+
+                //IRecord row = cursor.Single();
+                List<IRecord> rows=cursor.ToList();
+                //IReadOnlyList<string> columns = row.Keys;
+
+                //string city=row.Values.ContainsKey("")
+                
+                var records = cursor.ToList();
+                return records.Select(x => x["city"].As<string>());
+            });
+            var lista2 = res2.ToList();
             return null;
         }
     }
