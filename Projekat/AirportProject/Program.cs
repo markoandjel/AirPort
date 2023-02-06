d81fec1009b1d9397e1b23fe99e896978bc1a3b5
markoandjel <60831775+markoandjel@users.noreply.github.com>
2023-02-06 22:28:10 +0100

    dodavanje login forme koju je Sale pravio

--- Projekat/AirportProject/Program.cs
+++ Projekat/AirportProject/Program.cs
@@ -22,7 +22,8 @@ namespace AirportProject
             
             Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new Form1());
+                //Application.Run(new Form1());
+                Application.Run(new LogInForm());
         }
     }
 }
