49c2ce25dd51adb5af9c5b9544b7375843a40d68
markoandjel <60831775+markoandjel@users.noreply.github.com>
2023-02-06 22:16:09 +0100

    dodavanje create flight naredbe

--- Projekat/AirportProject/FlightForm.Designer.cs
+++ Projekat/AirportProject/FlightForm.Designer.cs
@@ -42,7 +42,15 @@
             this.dtpArival = new System.Windows.Forms.DateTimePicker();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numSeats = new System.Windows.Forms.NumericUpDown();
+            this.numPrice = new System.Windows.Forms.NumericUpDown();
+            this.dtpArrivalTime = new System.Windows.Forms.DateTimePicker();
+            this.dtpDepartureTime = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAirports)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSeats)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPrice)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvAirports
@@ -50,15 +58,15 @@
             this.dgvAirports.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvAirports.Location = new System.Drawing.Point(447, 78);
             this.dgvAirports.Name = "dgvAirports";
-            this.dgvAirports.Size = new System.Drawing.Size(315, 288);
+            this.dgvAirports.Size = new System.Drawing.Size(383, 288);
             this.dgvAirports.TabIndex = 0;
             this.dgvAirports.RowHeaderMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvAirports_RowHeaderMouseDoubleClick);
             // 
             // btnCreateFlight
             // 
-            this.btnCreateFlight.Location = new System.Drawing.Point(124, 356);
+            this.btnCreateFlight.Location = new System.Drawing.Point(129, 372);
             this.btnCreateFlight.Name = "btnCreateFlight";
-            this.btnCreateFlight.Size = new System.Drawing.Size(105, 37);
+            this.btnCreateFlight.Size = new System.Drawing.Size(100, 37);
             this.btnCreateFlight.TabIndex = 1;
             this.btnCreateFlight.Text = "Create Flight";
             this.btnCreateFlight.UseVisualStyleBackColor = true;
@@ -112,7 +120,7 @@
             // 
             this.lblListOf.AutoSize = true;
             this.lblListOf.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblListOf.Location = new System.Drawing.Point(546, 55);
+            this.lblListOf.Location = new System.Drawing.Point(580, 55);
             this.lblListOf.Name = "lblListOf";
             this.lblListOf.Size = new System.Drawing.Size(130, 20);
             this.lblListOf.TabIndex = 4;
@@ -120,7 +128,7 @@
             // 
             // btnShowAirports
             // 
-            this.btnShowAirports.Location = new System.Drawing.Point(483, 384);
+            this.btnShowAirports.Location = new System.Drawing.Point(496, 384);
             this.btnShowAirports.Name = "btnShowAirports";
             this.btnShowAirports.Size = new System.Drawing.Size(105, 37);
             this.btnShowAirports.TabIndex = 1;
@@ -130,7 +138,7 @@
             // 
             // btnShowFlights
             // 
-            this.btnShowFlights.Location = new System.Drawing.Point(624, 384);
+            this.btnShowFlights.Location = new System.Drawing.Point(665, 384);
             this.btnShowFlights.Name = "btnShowFlights";
             this.btnShowFlights.Size = new System.Drawing.Size(105, 37);
             this.btnShowFlights.TabIndex = 1;
@@ -142,14 +150,14 @@
             // 
             this.dtpDeparture.Location = new System.Drawing.Point(124, 211);
             this.dtpDeparture.Name = "dtpDeparture";
-            this.dtpDeparture.Size = new System.Drawing.Size(200, 20);
+            this.dtpDeparture.Size = new System.Drawing.Size(174, 20);
             this.dtpDeparture.TabIndex = 7;
             // 
             // dtpArival
             // 
             this.dtpArival.Location = new System.Drawing.Point(124, 257);
             this.dtpArival.Name = "dtpArival";
-            this.dtpArival.Size = new System.Drawing.Size(200, 20);
+            this.dtpArival.Size = new System.Drawing.Size(174, 20);
             this.dtpArival.TabIndex = 8;
             // 
             // label2
@@ -170,13 +178,71 @@
             this.label3.TabIndex = 9;
             this.label3.Text = "Time of arival:";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Price in €";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 338);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Seats";
+            // 
+            // numSeats
+            // 
+            this.numSeats.Location = new System.Drawing.Point(129, 333);
+            this.numSeats.Name = "numSeats";
+            this.numSeats.Size = new System.Drawing.Size(100, 20);
+            this.numSeats.TabIndex = 12;
+            // 
+            // numPrice
+            // 
+            this.numPrice.Location = new System.Drawing.Point(129, 307);
+            this.numPrice.Name = "numPrice";
+            this.numPrice.Size = new System.Drawing.Size(100, 20);
+            this.numPrice.TabIndex = 13;
+            // 
+            // dtpArrivalTime
+            // 
+            this.dtpArrivalTime.CustomFormat = "HH:mm";
+            this.dtpArrivalTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpArrivalTime.Location = new System.Drawing.Point(304, 258);
+            this.dtpArrivalTime.Name = "dtpArrivalTime";
+            this.dtpArrivalTime.ShowUpDown = true;
+            this.dtpArrivalTime.Size = new System.Drawing.Size(56, 20);
+            this.dtpArrivalTime.TabIndex = 7;
+            // 
+            // dtpDepartureTime
+            // 
+            this.dtpDepartureTime.CustomFormat = "HH:mm";
+            this.dtpDepartureTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpDepartureTime.Location = new System.Drawing.Point(304, 211);
+            this.dtpDepartureTime.Name = "dtpDepartureTime";
+            this.dtpDepartureTime.ShowUpDown = true;
+            this.dtpDepartureTime.Size = new System.Drawing.Size(56, 20);
+            this.dtpDepartureTime.TabIndex = 7;
+            // 
             // FlightForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(895, 450);
+            this.Controls.Add(this.numPrice);
+            this.Controls.Add(this.numSeats);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.dtpArival);
+            this.Controls.Add(this.dtpDepartureTime);
+            this.Controls.Add(this.dtpArrivalTime);
             this.Controls.Add(this.dtpDeparture);
             this.Controls.Add(this.btnSwapDestinations);
             this.Controls.Add(this.lblFlightFrom);
@@ -193,6 +259,8 @@
             this.Text = "FlightForm";
             this.Load += new System.EventHandler(this.FlightForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvAirports)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSeats)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPrice)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -211,8 +279,14 @@
         private System.Windows.Forms.Button btnShowAirports;
         private System.Windows.Forms.Button btnShowFlights;
         private System.Windows.Forms.DateTimePicker dtpDeparture;
-        private System.Windows.Forms.DateTimePicker dtpArival;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numSeats;
+        private System.Windows.Forms.NumericUpDown numPrice;
+        private System.Windows.Forms.DateTimePicker dtpArrivalTime;
+        private System.Windows.Forms.DateTimePicker dtpDepartureTime;
+        public System.Windows.Forms.DateTimePicker dtpArival;
     }
 }
