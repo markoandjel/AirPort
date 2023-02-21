7d8dc217d4ae431d3f0e68243ce945ee508024ef
aleksandarzaharijev <94053194+aleksandarzaharijev@users.noreply.github.com>
2023-02-21 17:11:31 +0100

    neki register zapocet

---
+++ Projekat/AirportProject/RegisterForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AirportProject
+{
+    public partial class RegisterForm : Form
+    {
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
