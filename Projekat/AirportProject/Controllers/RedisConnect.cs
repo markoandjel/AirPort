75f3144a186f1b348f65bec3c208b5e354fe8d21
aleksandarzaharijev <94053194+aleksandarzaharijev@users.noreply.github.com>
2023-03-02 22:34:18 +0100

    redis povezan, kreiran user entitet, radi, probajte

---
+++ Projekat/AirportProject/Controllers/RedisConnect.cs
@@ -0,0 +1,31 @@
+﻿using StackExchange.Redis;
+
+namespace AirportProject.Controllers
+{
+    public class RedisConnect
+    {
+        private readonly string connectionString;
+        private ConnectionMultiplexer connection;
+
+        public RedisConnect(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public bool Connect()
+        {
+            connection = ConnectionMultiplexer.Connect(connectionString);
+            return connection.IsConnected;
+        }
+
+        public IDatabase GetDatabase(int db = -1)
+        {
+            return connection.GetDatabase(db);
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+}
