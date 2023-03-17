8187ebdd04760e47bd36e142e34b8efe4862b8ce
markoandjel <60831775+markoandjel@users.noreply.github.com>
2023-03-17 23:01:31 +0100

    ispravka za login

--- Projekat/AirportProject/LogInForm.cs
+++ Projekat/AirportProject/LogInForm.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
-using Konscious.Security.Cryptography;
 
 using System.Net;
 using AirportProject.Controllers;
